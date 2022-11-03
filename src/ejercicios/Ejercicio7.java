218188266868a7c2f5489018ec8cc84d591b7884
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-03 14:08:37 +0100

    Añadidos comentarios Extras y cambiadas algunas Variables

--- src/ejercicios/Ejercicio7.java
+++ src/ejercicios/Ejercicio7.java
@@ -7,15 +7,16 @@ public class Ejercicio7 {
     public static void main(String[] args) {
         // ? Pedir un numero y calcular su factorial mediante un bucle for
         // ^ Declaramos la variable n (numero introducido) y resultado
-        long n, resultado = 1;
-        long nAux; // * Creamos esta variable para poderla usar en el resultado
+        long resultado = 1;
+        byte n; // * Elegí byte ya que más allá de 20! se sobrepasa el límite de 64 bits de Long
+        byte nAux; // * Creamos esta variable para poderla usar en el resultado
 
         // ^ Creamos el Scanner en la variable read y cambiamos su formato a US
         Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
         // ! Pedimos a el usuario el numero
         System.out.print("Introduce un número: ");
-        n = read.nextLong();
+        n = read.nextByte();
 
         // ! Igualamos las variables
         nAux = n;
