c11c8a180cea692241c90e76b4e679e6f357282f
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-08 11:11:23 +0100

    Terminado

--- src/ejercicios/Ejercicio9.java
+++ src/ejercicios/Ejercicio9.java
@@ -7,27 +7,25 @@ public class Ejercicio9 {
     public static void main(String[] args) {
         // ? Programa Java que pida un numero y muestre si es primo o no
         // ^ Declaramos la variable n (numero introducido)
-        long n;
+        long n, i;
         boolean esPrimo = true;
 
         // ^ Creamos el Scanner en la variable read y cambiamos su formato a US
         Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
-        // ! Pedimos a el usuario el numero
+        // ! Pedimos a el usuario el numero a comprobar
         System.out.print("Introduce un número: ");
         n = read.nextLong();
 
         // ? Salto de Línea Triple
         System.out.println("\n" + "-------------------------------" + "\n");
 
-        // ! Bucle for en el cual primero comprobaremos si es par diferente de 2, de ser así no es primo. Luego comprobaremos si siendo impar es divisible entre 3, 5, 7 y 11. Si es divisible por alguno de ellos no es primo.
-        for (byte i = 2; i <= 11; i++) {
-            if (n % i == 0 && n != i){ // * Si el numero es divisible entre i y no es igual a i
-                esPrimo = false; // * Cambiamos el valor de esPrimo a false
-            }
+        // ! Bucle for en el cual comprobamos si el numero es primo o no, si su modulo es 0 no es primo. De esta manera dividiremos el numero por todos los numeros hasta llegar al suyo mismo
+        for (i = 2; i < n; i++) {
+            if (n % i == 0 && n != i); esPrimo = false; // * Si el numero es divisible entre otro numero y no es el mismo, no es primo
         }
         
-        // ! Mostramos si el numero es primo o no
+        // ! Mostramos si el numero es primo o no mediante un operador ternario
         System.out.println(esPrimo ? "El número " + n + " es primo" : "El número " + n + " no es primo");
     }    
 }
